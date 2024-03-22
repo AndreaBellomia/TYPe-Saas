7395600079dd2bffe55baff265c1a6595566c89b
Andrea Bellomia <119261787+AndreaBellomia@users.noreply.github.com>
2024-03-22 13:27:51 +0100

    add run commands

---
+++ run.sh
@@ -0,0 +1,28 @@
+start_django() {
+    echo "Starting Django server..."
+    poetry run ./api-server/manage.py "$@"
+}
+
+start_nextjs() {
+    echo "Starting Next.js server..."
+    npm run dev --prefix ./frontend-server
+}
+
+stop_processes() {
+    echo "Stopping Django and Next.js servers..."
+    pkill -f "python manage.py runserver"
+    pkill -f "npm run dev"
+}
+
+trap stop_processes EXIT
+
+
+if [ "$1" = "runserver" ]; then
+    start_django "${@:1}" &
+    start_nextjs 2>&1 | sed 's/^/[Next  ] /'
+else
+    start_django "$@" &
+fi
+
+
+wait
